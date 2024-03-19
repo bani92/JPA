87ef1e01620ffe0ffbffbe0c094a5c825dfb5dbc
banseok <banseok9204@naver.com>
2024-03-19 21:21:25 +0900

    DTO 생성하여 반환

--- src/main/java/com/example/tdd/Test/MembershipService.java
+++ src/main/java/com/example/tdd/Test/MembershipService.java
@@ -3,13 +3,15 @@ package com.example.tdd.Test;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 @RequiredArgsConstructor
 public class MembershipService {
 
     private final MembershipRepository membershipRepository;
 
-    public Membership addMembership(final String userId, final MembershipType membershipType, final Integer point) {
+    public MembershipResponse addMembership(final String userId, final MembershipType membershipType, final Integer point) {
         final Membership result = membershipRepository.findByUserIdAndMembershipType(userId, membershipType);
         if(result != null) {
             throw new MembershipException(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER);
@@ -21,6 +23,11 @@ public class MembershipService {
                 .membershipType(membershipType)
                 .build();
 
-        return membershipRepository.save(membership);
+        final Membership saveMembership = membershipRepository.save(membership);
+
+        return MembershipResponse.builder()
+                .id(saveMembership.getId())
+                .membershipType(saveMembership.getMembershipType())
+                .build();
     }
 }
