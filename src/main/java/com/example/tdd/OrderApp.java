715841f1f97a43dd8d4cb3331e86baf8ea5c62cf
banseok <banseok9204@naver.com>
2024-04-26 21:23:52 +0900

    스프링 컨테이너 생성

--- src/main/java/com/example/tdd/OrderApp.java
+++ src/main/java/com/example/tdd/OrderApp.java
@@ -8,15 +8,20 @@ import com.example.tdd.order.AppConfig;
 import com.example.tdd.order.Order;
 import com.example.tdd.order.OrderService;
 import com.example.tdd.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
-//        MemberService memberService = new MemberServiceImpl();
-//        OrderService orderService = new OrderServiceImpl();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext =  new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
