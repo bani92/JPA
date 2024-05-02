4c504529c1c5f3873779c32f9ee388b19ceba82d
banseok <banseok9204@naver.com>
2024-05-02 21:14:46 +0900

    스프링 싱글톤 패턴 @Configuration (CGLIB)

---
+++ src/test/java/com/example/tdd/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,39 @@
+package com.example.tdd.singleton;
+
+import com.example.tdd.member.MemberRepository;
+import com.example.tdd.member.MemberServiceImpl;
+import com.example.tdd.order.AppConfig;
+import com.example.tdd.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemberRepository memberRepository = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("memberRepository2 = " + memberRepository2);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+
+        // @Bean이 붙은 메서드마다 이미 스프링 빈이 존재하면 존재하는 빈을 반환
+        // 스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다
+    }
+}
