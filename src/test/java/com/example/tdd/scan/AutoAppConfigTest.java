4b4f24d929e0b8b1a549b1fbc64816fb9c22d8ed
banseok <banseok9204@naver.com>
2024-05-05 13:29:59 +0900

    스프링 의존관계 생성자 주입

--- src/test/java/com/example/tdd/scan/AutoAppConfigTest.java
+++ src/test/java/com/example/tdd/scan/AutoAppConfigTest.java
@@ -1,7 +1,9 @@
 package com.example.tdd.scan;
 
+import com.example.tdd.member.MemberRepository;
 import com.example.tdd.member.MemberService;
 import com.example.tdd.AutoAppConfig;
+import com.example.tdd.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,6 +15,10 @@ public class AutoAppConfigTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        Assertions.assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
 
 }
