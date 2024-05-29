e7dcd661071d769d40c8d0db243dad14ac3ddd85
banseok <banseok9204@naver.com>
2024-05-30 08:38:55 +0900

    JPA 퀵스타트 - 6.1.5 JPQL 번호 바인딩

--- src/main/java/com/example/tdd/jpa4/JPQLBasicClient.java
+++ src/main/java/com/example/tdd/jpa4/JPQLBasicClient.java
@@ -21,16 +21,27 @@ public class JPQLBasicClient {
     private static void dataSelect(EntityManagerFactory emf) {
         EntityManager em = emf.createEntityManager();
 
+        String jpql = "SELECT id, name, title, deptName, salary " + "FROM Employee WHERE id = ?1 AND name = ?2";
+
+        Query query = em.createQuery(jpql);
+        query.setParameter(1, 1L);
+        query.setParameter(2, "직원 1");
+
+        Object[] result = (Object[]) query.getSingleResult();
+        System.out.println(result[0] + "번 직원의 정보");
+        System.out.println(Arrays.toString(result));
+
+
         // JPQL
 //        String jpql = "SELECT id, name, deptName, salary FROM Employee";
 
         // NEW 생성자 사용시 패키지 경로가 포함된 전체 경로를 지정
-        String jpql = "SELECT " + "NEW com.example.tdd.jpa4.EmployeeSalaryData(id, salary, " +
-                "commissionPct) FROM Employee";
-
-        TypedQuery<EmployeeSalaryData> query = em.createQuery(jpql, EmployeeSalaryData.class);
-
-        List<EmployeeSalaryData> resultList = query.getResultList();
+//        String jpql = "SELECT " + "NEW com.example.tdd.jpa4.EmployeeSalaryData(id, salary, " +
+//                "commissionPct) FROM Employee";
+//
+//        TypedQuery<EmployeeSalaryData> query = em.createQuery(jpql, EmployeeSalaryData.class);
+//
+//        List<EmployeeSalaryData> resultList = query.getResultList();
 
 
         // JPQL 전송 - 영속 컨테이너에 전송할 JPQL , JPQL 실행 결과를 매핑할 엔티티타입
@@ -40,10 +51,10 @@ public class JPQLBasicClient {
         //List<Employee> resultList = query.getResultList();
 
         // 검색 결과 처리
-        System.out.println("검색된 직원 목록");
-        for (EmployeeSalaryData result : resultList) {
-            System.out.println("---> " + result.toString());
-        }
+//        System.out.println("검색된 직원 목록");
+//        for (EmployeeSalaryData result : resultList) {
+//            System.out.println("---> " + result.toString());
+//        }
         em.close();
     }
 
